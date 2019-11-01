4d8bbdfdddd730b32b88d5b3b66e0fce50b39a40
derek <derek_merck@brown.edu>
2019-11-01 13:18:31 -0400

    Enable factory create observalbes for siren skip healthcheck on ls option

--- package/crud/cli/commands/ls.py
+++ package/crud/cli/commands/ls.py
@@ -2,8 +2,9 @@ import click
 
 
 @click.command(short_help="List all services and health")
+@click.option("--health-check/--skip-health-check", "-h/-k", default=True, help="Skip health")
 @click.pass_context
-def ls(ctx):
+def ls(ctx, health_check):
     """List all services and health
 
     \b
@@ -12,5 +13,8 @@ def ls(ctx):
     click.echo(click.style('List Services and Health', underline=True, bold=True))
 
     for ep in ctx.obj["services"].get_all():
-        avail = ep.check()
+        if health_check:
+            avail = ep.check()
+        else:
+            avail = "Unknown"
         click.echo("{}: {}".format(ep.name, avail))
